71ba302dcce9a33438d6feac066498430b1d3f7f
Jean Quilbeuf <jean.quilbeuf@huawei.com>
2023-11-05 11:57:03 +0000

    make clear that validate doesn’t work

--- yang/schema_mount_based/validate_data.sh
+++ yang/schema_mount_based/validate_data.sh
@@ -3,6 +3,7 @@
 if [[ $1 == "" ]]
 then
  print "Usage: ./validate_data.sh <data file>"
+ exit 1
 fi
 
 yanglint -f xml  -p yang  \
@@ -10,3 +11,7 @@ yanglint -f xml  -p yang  \
   -Y network-level-yanglib.xml \
   -m extension-data.xml \
   $1
+
+echo
+echo
+echo "NOT WORKING YET, WIP"
